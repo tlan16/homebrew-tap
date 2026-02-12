4bc14b9f596c0983ff5664eba57b8fa9fb443312
Frank Lan <franklan118@gmail.com>
2026-02-13 10:54:16 +1100

    AI: update burno to hack it to work

--- Formula/bruno.rb
+++ Formula/bruno.rb
@@ -1,27 +1,25 @@
-class MacCron < Formula
+class Bruno < Formula
   desc "bruno"
   homepage "https://github.com/usebruno/bruno"
   url "https://github.com/usebruno/bruno.git", branch: "release/v3.0.2"
   version "v3.0.2"
-  url "file://packages/bruno-electron/out/bruno_2.0.0_arm64_mac.dmg"
+  head "https://github.com/usebruno/bruno.git", branch: "main"
 
   depends_on "node" => :build
 
   def install
-    system "npm", "--frozen-lockfile"
+    system "npm", "install"
     system "npm", "run", "build:electron:mac"
+
+    # Install the app
+    app_files = Dir["packages/bruno-electron/out/**/Bruno.app"]
+    raise "Could not find Bruno.app" if app_files.empty?
+
+    prefix.install app_files.first
   end
 
   livecheck do
     url :url
     strategy :github_latest
   end
-
-  app "Cast.app"
-
-  zap trash: [
-    "~/Library/Application Support/Cast",
-    "~/Library/Preferences/com.example.cast.plist",
-    "~/Library/Caches/com.example.cast",
-  ]
 end
